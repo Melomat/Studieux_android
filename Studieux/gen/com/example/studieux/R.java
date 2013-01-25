6e216ddcac756af0f7ec5eadad8b29457bf51860
Louis D. <link14.online@gmail.com>
2013-01-25 13:10:56 +0100

    ajout sliding menu

--- Studieux/gen/com/example/studieux/R.java
+++ Studieux/gen/com/example/studieux/R.java
@@ -121,29 +121,43 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          */
         public static final int viewBehind=0x7f010002;
     }
+    public static final class color {
+        public static final int gray_menu=0x7f050000;
+    }
+    public static final class dimen {
+        public static final int list_padding=0x7f060001;
+        public static final int shadow_width=0x7f060002;
+        public static final int slidingmenu_offset=0x7f060000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int shadow=0x7f020001;
     }
     public static final class id {
+        public static final int button1=0x7f040005;
         public static final int fullscreen=0x7f040003;
         public static final int left=0x7f040000;
         public static final int margin=0x7f040002;
-        public static final int menu_settings=0x7f040006;
+        public static final int menu_settings=0x7f040007;
         public static final int right=0x7f040001;
         public static final int selected_view=0x7f040004;
-        public static final int slidingmenumain=0x7f040005;
+        public static final int slidingmenumain=0x7f040006;
+    }
+    public static final class integer {
+        public static final int num_cols=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int slidingmenumain=0x7f030001;
+        public static final int sliding_menu=0x7f030001;
+        public static final int slidingmenumain=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_main=0x7f0a0000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
+        public static final int app_name=0x7f080000;
+        public static final int hello_world=0x7f080001;
+        public static final int menu_settings=0x7f080002;
     }
     public static final class style {
         /** 
@@ -167,11 +181,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f090001;
     }
     public static final class styleable {
         /** Attributes that can be used with a SlidingMenu.
